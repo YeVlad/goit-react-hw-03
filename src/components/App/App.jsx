0e8ec4145144f105c1632918fad8398743bacc6d
YeVlad <150618209+YeVlad@users.noreply.github.com>
2024-05-18 19:57:35 +0200

    add "Add" function

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -9,12 +9,24 @@ import ContactList from "../contactList/contactList";
 
 function App() {
   const [contacts, setContacts] = useState(dateContacts);
+  const [search, setSearch] = useState("");
+
+  const filterContacts = contacts.filter((contact) =>
+    contact.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  function addContact(obj) {
+    setContacts((prevContacts) => {
+      return [...prevContacts, obj];
+    });
+  }
+
   return (
     <>
       <h1>Phonebook</h1>
-      <ContactForm />
-      <SearchBox />
-      <ContactList contacts={contacts} />
+      <ContactForm addContact={addContact} />
+      <SearchBox value={search} onSearch={setSearch} />
+      <ContactList contacts={filterContacts} />
     </>
   );
 }
--- src/components/contactForm/contactForm.jsx
+++ src/components/contactForm/contactForm.jsx
@@ -1,16 +1,33 @@
 import css from "./contactForm.module.css";
 
 import { Formik, Form, Field } from "formik";
+import { nanoid } from "nanoid";
 
-export default function ContactForm() {
+export default function ContactForm({ addContact }) {
+  function handleSubmit(values, actions) {
+    addContact({
+      id: nanoid(),
+      name: values.name,
+      number: values.number,
+    });
+    actions.resetForm();
+  }
   return (
-    <Formik initialValues={{}} onSubmit={() => {}}>
+    <Formik
+      initialValues={{
+        name: "",
+        number: "",
+      }}
+      onSubmit={handleSubmit}
+    >
       <Form className={css.addContact}>
         <label htmlFor="name">Name</label>
         <Field type="text" name="name" className={css.formField}></Field>
         <label htmlFor="number">Number</label>
         <Field type="text" name="number" className={css.formField}></Field>
-        <button className={css.addBut}>Add contact</button>
+        <button className={css.addBut} type="submit">
+          Add contact
+        </button>
       </Form>
     </Formik>
   );
